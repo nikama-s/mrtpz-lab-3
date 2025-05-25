8c54cacd4aff1419b20ffd16dd6853792f645a92
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2025-05-26 02:40:51 +0300

    add simple js project

---
+++ Js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const colors = require("colors");
+
+console.log(colors.red("Every"));
+console.log(colors.yellow("Hunter"));
+console.log(colors.green("Wants"));
+console.log(colors.blue("To"));
+console.log(colors.magenta("Know"));
+console.log(colors.cyan("Where"));
+console.log(colors.white("The"));
+console.log(colors.rainbow("Phoenix"));
+console.log(colors.gray("Nests"));
